753682e4a9b618c6f0c8d46cca10f59b4f00d980
Burak Seyhan <seyhanburak9@gmail.com>
2020-01-09 16:43:02 +1100

    new notes with multiply with map function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
+import App from "./components/App";
 
-// This is a comment for testing new keeper-john branch
-// Second comment added by john
-
-ReactDOM.render(
-  <div>
-    <Header />
-    <Note />
-    <Footer />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
