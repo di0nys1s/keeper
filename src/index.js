f94bd4e80f4a4ca0e3ee8843ddcee035a5a1dd4f
Burak Seyhan <seyhanburak9@gmail.com>
2020-01-09 13:36:17 +1100

    comment added in master branch into index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 
+// This is the test comment added in master branch
 ReactDOM.render(
   <div>
     <Header />
